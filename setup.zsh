03c3e7d51e768f1e2dcf9893a72a07bd195a5f59
Thomas Schneider <thosch97@gmail.com>
2015-02-15 16:44:52 +0100

    Don’t break on FreeBSD
    Signed-off-by: Thomas Schneider <thosch97@gmail.com>

--- setup.zsh
+++ setup.zsh
@@ -2,6 +2,16 @@
 
 [[ $PWD = ~/.dotfiles ]] || exit 1
 
+if [[ $(uname -s) = FreeBSD ]]; then
+	if [[ -x /compat/linux/bin/ln ]]; then
+		LN=/compat/linux/bin/ln
+	else
+		echo "On FreeBSD and no GNU ln found, exiting"
+		exit 1
+	fi
+fi
+LN=${LN:-ln}
+
 for i in dot.*; do
-	ln -Ts .dotfiles/${i} ~/${i#dot}
+	${LN} -Ts .dotfiles/${i} ~/${i#dot}
 done
