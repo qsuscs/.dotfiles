615671876fb35365c98808e860aac052d647d6c0
Thomas Schneider <qsx@qsx.re>
2017-07-18 17:25:14 +0200

    Emacs: various customizations
    Signed-off-by: Thomas Schneider <qsx@qsx.re>

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -1,5 +1,4 @@
 (package-initialize)
-
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.org/packages/") t)
@@ -15,24 +14,23 @@
 
 (server-start)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (sanityinc-tomorrow-blue)))
- '(custom-safe-themes
-   (quote
-    ("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" default)))
- '(package-selected-packages (quote (color-theme-sanityinc-tomorrow)))
- '(scroll-bar-adjust-thumb-portion nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file
+      (concat user-emacs-directory
+	      (convert-standard-filename "custom.el")))
+(load custom-file :noerror)
 
 (use-package color-theme-sanityinc-tomorrow
   :ensure t
   :config (color-theme-sanityinc-tomorrow-blue))
+
+(use-package yaml-mode
+  :ensure t
+  :mode ("\\.yml\\'" . yaml-mode))
+
+(use-package linum-relative
+  :ensure t)
+(linum-mode)
+(linum-relative-global-mode)
+
+(add-to-list 'auto-mode-alist '("/mutt" . mail-mode))
+(add-hook 'mail-mode-hook '(lambda() (turn-on-auto-fill)))
