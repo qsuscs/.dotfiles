30ea70cdd3fb2928b8268d133a05aaa68b59baca
Thomas Schneider <qsx@qsx.re>
2017-07-18 09:32:34 +0200

    Clean Emacs config
    Signed-off-by: Thomas Schneider <qsx@qsx.re>

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -1,11 +1,38 @@
+(package-initialize)
+
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives
 	     '("gnu" . "http://elpa.gnu.org/packages/") t)
-(add-to-list 'auto-mode-alist '("\\.doc$" . doc-mode))
-(add-to-list 'load-path "~/.emacs.d/elisp/")
-(add-to-list 'auto-mode-alist '("\\.asciidoc$" . doc-mode))
-(autoload 'doc-mode "doc-mode")
-;(load-library "auctex_custom")
+
+(let ((default-directory
+	(concat user-emacs-directory
+		(convert-standard-filename "elisp/"))))
+  (normal-top-level-add-subdirs-to-load-path))
+
+(require 'use-package)
+
 (server-start)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-blue)))
+ '(custom-safe-themes
+   (quote
+    ("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" default)))
+ '(package-selected-packages (quote (color-theme-sanityinc-tomorrow)))
+ '(scroll-bar-adjust-thumb-portion nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(use-package color-theme-sanityinc-tomorrow
+  :ensure t
+  :config (color-theme-sanityinc-tomorrow-blue))
