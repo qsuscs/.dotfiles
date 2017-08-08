d47b9983528647d1862169ef3f8446131385f509
Thomas Schneider <qsx@qsx.re>
2017-08-08 16:18:41 +0200

    setup.py: Apply some of TobiX’ modifications
    Signed-off-by: Thomas Schneider <qsx@qsx.re>

--- setup.py
+++ setup.py
@@ -8,13 +8,18 @@ os.chdir(os.path.dirname(__file__))
 exit = 0
 
 for f in glob('dot.*'):
-    dst = os.path.expanduser('~/' + f[3:])
+    dst = os.path.expanduser(
+        '~/' + f[3:].replace("--", "\ufffd").replace("-", "/").replace("\ufffd", "-"))
     src = os.path.join(os.getcwd(), f)
+    src_rel = os.path.relpath(src, os.path.dirname(dst))
+
+    os.makedirs(os.path.dirname(dst), exist_ok=True)
+
     try:
-        os.symlink(src, dst)
+        os.symlink(src_rel, dst)
     except FileExistsError:
         if not os.path.samefile(src, dst):
-            print(dst + " exists and does not link do " + dst)
+            print(dst + " exists and does not link do " + src)
             exit = 1
 
 sys.exit(exit)
