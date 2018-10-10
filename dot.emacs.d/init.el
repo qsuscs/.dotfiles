a373cd3ec9cc13179fb389db38990a5cacc606cf
Thomas Schneider <qsx@qsx.re>
2018-10-10 11:39:40 +0200

    Emacs: Fix C-M-y

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -94,7 +94,7 @@
 (global-set-key (kbd "C-x C-k") 'kill-this-buffer)
 (global-set-key (kbd "C-M-y") '(lambda ()
 				 (interactive)
-				 (save-excursion (insert (gui-get-primary-selection)))))
+				 (insert (gui-get-primary-selection))))
 
 ;;; Mail
 (setq message-kill-buffer-on-exit t
