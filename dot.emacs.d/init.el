8ee5e4161e090a89dc1af0ea35fe895a72d399dd
Thomas Schneider <qsx@chaotikum.eu>
2021-07-13 18:23:39 +0200

    Emacs: Set use-package-verbose

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -9,7 +9,8 @@
   (package-refresh-contents)
   (package-install 'use-package t))
 (require 'use-package)
-(setq use-package-always-ensure t)
+(setq use-package-always-ensure t
+      use-package-verbose t)
 
 (defvar --backup-directory (concat user-emacs-directory "backups"))
 (if (not (file-exists-p --backup-directory))
