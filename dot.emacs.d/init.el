5f87cc5242b9ec1619ec9124bd87b2fcc62dc922
Thomas Schneider <qsx@chaotikum.eu>
2026-07-27 19:57:09 +0200

    Emacs: `kill-this-buffer` → `kill-current-buffer`

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -426,7 +426,7 @@
 
 (setq column-number-mode t)
 
-(global-set-key (kbd "C-x C-k") #'kill-this-buffer)
+(global-set-key (kbd "C-x C-k") #'kill-current-buffer)
 (global-set-key (kbd "C-M-y") #'(lambda ()
 				  (interactive)
 				  (insert (gui-get-primary-selection))))
